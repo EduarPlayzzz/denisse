5da6d5c82dfcdb0806af9d44efd059a8088a8c87
EduarPlayzzz <111006306+EduarPlayzzz@users.noreply.github.com>
2024-09-01 22:27:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+particlesJS("body", {
+    "particles": {
+      "number": {
+        "value": 50
+      },
+      "shape": {
+        "type": "image",
+        "image": {
+          "src": "images/heart.png", // Cambia a la ruta correcta de tu imagen
+          "width": 100,
+          "height": 100
+        }
+      },
+      "size": {
+        "value": 10
+      },
+      "move": {
+        "speed": 2,
+        "direction": "bottom",
+        "out_mode": "out"
+      }
+    }
+  });
+  
+  
